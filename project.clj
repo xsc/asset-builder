c48275d2800d83ec857b5436a2e6254826e60662
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-02-01 10:56:29 +0100

    Version 0.2.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject xsc/asset-builder "0.2.1"
+(defproject xsc/asset-builder "0.2.2-SNAPSHOT"
   :description "Utility for CLJS + Asset Building"
   :url "https://github.com/xsc/asset-builder"
   :license {:name "MIT License"
